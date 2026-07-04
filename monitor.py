40408224e57a1750fb27630c9f54e0dc86f80bd0
banjofag2001-lab <banjofag2001@gmail.com>
2026-07-04 12:38:09 +0100

    Refactor hash storage functionality

--- monitor.py
+++ monitor.py
@@ -15,28 +15,42 @@ def save_hashes(hashes):
         json.dump(hashes, file, indent=4)
 
 
+def load_hashes():
+    if os.path.exists("hashes.json"):
+        with open("hashes.json", "r") as file:
+            return json.load(file)
+
+    return {}
+
+
 def scan_folder(folder_path):
     print("=" * 50)
     print("Security File Integrity Monitor")
     print("=" * 50)
     print("\nScanning folder...\n")
 
-    hashes = {}
+    old_hashes = load_hashes()
+    new_hashes = {}
 
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
 
         file_hash = calculate_hash(file_path)
 
-        hashes[file_name] = file_hash
+        new_hashes[file_name] = file_hash
 
-        print(f"Found: {file_name}")
-        print(f"SHA-256: {file_hash}\n")
+        if file_name not in old_hashes:
+            print(f"NEW FILE DETECTED: {file_name}")
 
-    save_hashes(hashes)
+        elif old_hashes[file_name] != file_hash:
+            print(f"WARNING: {file_name} has been modified!")
 
-    print("Hashes saved successfully.")
-    print("Scan completed.")
+        else:
+            print(f"OK: {file_name} has not changed")
+
+    save_hashes(new_hashes)
+
+    print("\nScan completed.")
 
 
 def main():
