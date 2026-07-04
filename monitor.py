87fd207bbe6947acc76e198cae69f64b86d73995
banjofag2001-lab <banjofag2001@gmail.com>
2026-07-04 12:31:12 +0100

    Refactor hash storage functionality

--- monitor.py
+++ monitor.py
@@ -2,11 +2,17 @@ import os
 import hashlib
 import json
 
+
 def calculate_hash(file_path):
     with open(file_path, "rb") as file:
         file_data = file.read()
 
     return hashlib.sha256(file_data).hexdigest()
+
+
+def save_hashes(hashes):
+    with open("hashes.json", "w") as file:
+        json.dump(hashes, file, indent=4)
 
 
 def scan_folder(folder_path):
@@ -15,32 +21,27 @@ def scan_folder(folder_path):
     print("=" * 50)
     print("\nScanning folder...\n")
 
-    print(f"Folder: {folder_path}\n")
+    hashes = {}
 
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
 
         file_hash = calculate_hash(file_path)
 
+        hashes[file_name] = file_hash
+
         print(f"Found: {file_name}")
         print(f"SHA-256: {file_hash}\n")
 
+    save_hashes(hashes)
+
+    print("Hashes saved successfully.")
     print("Scan completed.")
+
 
 def main():
     folder = "test_files"
-
-    hashes = {}
-
-    for file_name in os.listdir(folder):
-        file_path = os.path.join(folder, file_name)
-        hashes[file_name] = calculate_hash(file_path)
-
-    with open("hashes.json", "w") as file:
-        json.dump(hashes, file, indent=4)
-
-    print("Hashes saved successfully.")
-
+    scan_folder(folder)
 
 
 if __name__ == "__main__":
